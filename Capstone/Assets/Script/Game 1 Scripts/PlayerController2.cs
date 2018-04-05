83dd025c863a5b262b3c28f4c6ddc6fd6d67d129
ErickCundangan <erick.cundangan20@gmail.com>
2018-04-05 15:49:23 +0800

    Added game 1 stage 1 scene

---
+++ Capstone/Assets/Script/Game 1 Scripts/PlayerController2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour {
+	float distance = 10;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+
+	void Start() {
+
+	}
+
+	void Update() {
+		if (Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	void OnMouseDrag() {
+		Vector3 touchPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 playerPos = Camera.main.ScreenToWorldPoint (touchPos);
+		transform.position = new Vector3 (playerPos.x, -3, 0);
+	}
+}
