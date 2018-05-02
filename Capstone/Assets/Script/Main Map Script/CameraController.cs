1f7b36821df48d44c061ab1d685f4766acc8f5dd
ErickCundangan <erick.cundangan20@gmail.com>
2018-05-02 22:21:30 +0800

    Camera now does not leave map

--- Capstone/Assets/Script/Main Map Script/CameraController.cs
+++ Capstone/Assets/Script/Main Map Script/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour {
 	public GameObject followTarget;
 	private Vector3 targetPos;
 	public float moveSpeed;
+	public float minX, maxX, minY, maxY;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +17,18 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		targetPos = new Vector3 (followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+		if (followTarget.transform.position.x <= minX)
+			targetPos.x = minX;
+
+		if (followTarget.transform.position.x >= maxX)
+			targetPos.x = maxX;
+
+		if (followTarget.transform.position.y <= minY)
+			targetPos.y = minY;
+
+		if (followTarget.transform.position.y >= maxY)
+			targetPos.y = maxY;
+
 		transform.position = Vector3.Lerp (transform.position, targetPos, moveSpeed * Time.deltaTime);
 	}
 }
