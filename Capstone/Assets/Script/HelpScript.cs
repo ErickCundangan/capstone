e4ebb46d870b9f71e59d0b8adfa94741fe04010e
ErickCundangan <erick.cundangan20@gmail.com>
2018-05-27 16:12:44 +0800

    Added Help Button to every game

---
+++ Capstone/Assets/Script/HelpScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpScript : MonoBehaviour {
+
+	public GameObject tutorial;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpenHelp_OnClick() {
+		tutorial.SetActive (true);
+		Time.timeScale = 0;
+	}
+
+	public void CloseHelp_OnClick() {
+		tutorial.SetActive (false);
+		Time.timeScale = 1;
+	}
+}
